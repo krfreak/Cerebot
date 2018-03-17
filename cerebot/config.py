98994a3524699cf855a79731842d7bb1afa3cf9e
gammafunk <gammafunk@gmail.com>
2018-03-16 23:49:16 -0500

    Updates to bot config from beem
    Rename in the config class and updates to the default config due to some
    changes in gammafunk/beem@0008c607.

--- cerebot/config.py
+++ cerebot/config.py
@@ -1,28 +1,23 @@
 """Cerebot configuration data."""
 
-from beem.config import Config
+from beem.config import BotConfig
 
-class CerebotConfig(Config):
-    """Holds the LomLobot configuration data."""
-
-    def __init__(self, path):
-        super().__init__(path)
+class CerebotConfig(BotConfig):
+    """Handle configuration data loading for Cerebot."""
 
     def check_discord(self):
+        """Check that there is a 'discord' table in the TOML data and that it
+        has the necessary entries."""
+
         if not self.get("discord"):
             self.error("The discord table is undefined")
 
         self.require_table_fields("discord", self.discord, ["token"])
 
     def load(self):
-        """Read the main TOML configuration data from self.path and check
-        that the configuration is valid.
-
-        """
+        """Read the main TOML configuration data from self.path and check that
+        the configuration is valid."""
 
         super().load()
         self.check_dcss()
         self.check_discord()
-
-    def error(self, msg):
-        raise Exception("Config file {}: {}".format(self.path, msg))
